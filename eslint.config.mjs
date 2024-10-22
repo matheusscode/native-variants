6397243215418760b3d9cc4816c9722c83f234d1
matheusscode <matheussdev3@gmail.com>
2024-10-21 22:45:19 -0400

    feat: init setup config

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Disable the rule for 'any' type
+      "@typescript-eslint/no-empty-interface": "off", // Disable the empty interface rule
+      "@typescript-eslint/no-empty-object-type": "off", // Disable the empty object type rule
+    },
+  },
+];
